d5331ad2a0fe6f7e08c003afdce4a86f407e7c1f
nikhil8810 <36456442+nikhil8810@users.noreply.github.com>
2018-02-16 13:14:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/blog-one', function (req, res) {
-  res.send('Blog-one requested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'blog-one.html'));
 });
 
 app.get('/blog-two', function (req, res) {
