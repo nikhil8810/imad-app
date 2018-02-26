084c63c1385088115d41ee4d7e76493870d88ec2
nikhil8810 <36456442+nikhil8810@users.noreply.github.com>
2018-02-27 01:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
     request.open('GET', 'http://someonenikhilkr.imad.hasura-app.io', true);
     request.send(null);
 };
+
+//submit name
+var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick= function () {
+    //Make a request to the server and send name
+    
+    //capture a list of names and render it as a list.
+    var names = ['name 1','name 2','name 3'];
+    var list = '';
+    for (var i=0;i<names.length;i++) {
+        list+= '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+};
